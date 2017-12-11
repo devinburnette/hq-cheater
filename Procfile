da6369cfabc8cd53af3511e681c82d0fa5170d9e
Devin Burnette <devin.burnette@betterment.com>
2017-12-11 00:06:11 -0500

    try sanic

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python ./app.py $PORT
+web: python3 ./app.py $PORT
--- app.py
+++ app.py
@@ -1,32 +1,27 @@
 import re
-import json
+import json as jsn
 from sys import argv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from bottle import route, run, request, static_file
+from sanic import Sanic
+from sanic.response import text, json
+app = Sanic(__name__)
 
-@route('/')
-def index():
-    return static_file('index.html', root='./')
+app.static('/', './index.html')
+app.static('/css/style.css', './static/css/style.css')
+app.static('/js/speech.js', './static/js/speech.js')
+app.static('/js/query.js', './static/js/query.js')
 
-@route('/<filename:re:.*\.js>')
-def js(filename):
-    return static_file(filename, root='static/')
-
-@route('/<filename:re:.*\.css>')
-def css(filename):
-    return static_file(filename, root='static/')
-
-@route('/google')
-def google():
-    search = request.query.search
-    choices = request.query.choices
+@app.route('/google')
+async def google(request):
+    search = request.raw_args['search']
+    choices = request.raw_args['choices']
     ignore_list = ['or', 'and', 'the']
     sanitized = re.split(', | | or', choices)
     for word in ignore_list:
         if word in sanitized:
             sanitized.remove(word)
-    
+
     # Start Google
     driver = webdriver.Chrome('/usr/local/bin/chromedriver')
     driver.get('http://www.google.com')
@@ -40,9 +35,10 @@ def google():
     results = dict.fromkeys(sanitized, 0)
     previews = driver.find_elements_by_css_selector('span.st')
     for preview in previews:
-	for choice in sanitized:
-	    if choice.lower() in preview.get_attribute('innerHTML').lower():
-		results[choice] += 1
-    return json.dumps(results)
+        for choice in sanitized:
+            if choice.lower() in preview.get_attribute('innerHTML').lower():
+                results[choice] += 1
+    return json(jsn.dumps(results))
 
-run(host='0.0.0.0', port=argv[1])
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=argv[1])
